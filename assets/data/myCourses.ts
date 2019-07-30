c5a0f0ffbbbd5a26b38dbb981ed1903abb3d080a
nugangha1 <53315506+nugangha1@users.noreply.github.com>
2019-07-30 17:32:25 -0400

    Add files via upload

---
+++ assets/data/myCourses.ts
@@ -0,0 +1,12 @@
+import { Course } from '../../app/courses'
+
+// is in array of object inside data
+export const MYCOURSES: Course[] = [
+     
+        {cid: 'SYS2444', cname:' Mobile Web App', cprof: 'Addy' },
+
+        { cid: 'SYS24238', cname:' C# Web App', cprof: 'Happ' }
+         
+
+     
+];
---
+++ assets/data/profiledata.ts
@@ -0,0 +1,6 @@
+import { MyProfile } from '../../app/myprofile'
+
+export const MYPROFILE: MyProfile = {
+//define data
+    id:'998483', name: 'Patric', year: 4
+};
